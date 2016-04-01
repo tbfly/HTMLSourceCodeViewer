0a237b09a4ba2c202e278ffb476ae685658a8a68
Lu ZhiHe <luzhihe@gmail.com>
2016-04-01 15:24:32 +0800

    *): Init version for Neteast Lofter Url Parser.
    To get the protected picture download URL.

--- app/build/generated/source/r/debug/com/sbstrm/appirater/R.java
+++ app/build/generated/source/r/debug/com/sbstrm/appirater/R.java
@@ -8,12 +8,12 @@ package com.sbstrm.appirater;
 
 public final class R {
 	public static final class bool {
-		public static final int appirator_test_mode = 0x7f040000;
+		public static final int appirator_test_mode = 0x7f070000;
 	}
 	public static final class color {
-		public static final int appirator_button_end_color = 0x7f050000;
-		public static final int appirator_button_start_color = 0x7f050001;
-		public static final int appirator_button_text_color = 0x7f050002;
+		public static final int appirator_button_end_color = 0x7f080000;
+		public static final int appirator_button_start_color = 0x7f080001;
+		public static final int appirator_button_text_color = 0x7f080002;
 	}
 	public static final class drawable {
 		public static final int button_appirator = 0x7f020000;
@@ -25,25 +25,25 @@ public final class R {
 		public static final int rateLater = 0x7f0b0006;
 	}
 	public static final class integer {
-		public static final int appirator_days_before_reminding = 0x7f070000;
-		public static final int appirator_days_until_prompt = 0x7f070001;
-		public static final int appirator_delay_until_prompt = 0x7f070002;
-		public static final int appirator_events_until_prompt = 0x7f070003;
-		public static final int appirator_launches_until_prompt = 0x7f070004;
+		public static final int appirator_days_before_reminding = 0x7f090000;
+		public static final int appirator_days_until_prompt = 0x7f090001;
+		public static final int appirator_delay_until_prompt = 0x7f090002;
+		public static final int appirator_events_until_prompt = 0x7f090003;
+		public static final int appirator_launches_until_prompt = 0x7f090004;
 	}
 	public static final class layout {
 		public static final int appirater = 0x7f030002;
 	}
 	public static final class string {
-		public static final int appirator_app_title = 0x7f080003;
-		public static final int appirator_market_url = 0x7f080004;
-		public static final int rate = 0x7f08000b;
-		public static final int rate_cancel = 0x7f08000c;
-		public static final int rate_later = 0x7f08000d;
-		public static final int rate_message = 0x7f08000e;
-		public static final int rate_title = 0x7f08000f;
+		public static final int appirator_app_title = 0x7f040008;
+		public static final int appirator_market_url = 0x7f040009;
+		public static final int rate = 0x7f040000;
+		public static final int rate_cancel = 0x7f040001;
+		public static final int rate_later = 0x7f040002;
+		public static final int rate_message = 0x7f040003;
+		public static final int rate_title = 0x7f040004;
 	}
 	public static final class style {
-		public static final int appiratorButton = 0x7f090002;
+		public static final int appiratorButton = 0x7f060002;
 	}
 }
